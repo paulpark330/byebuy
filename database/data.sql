9a096e5a806f75c0e400a89ff089e09b3700a5cf
Paul Park <paulpark330@hotmail.com>
2021-09-07 17:42:28 -0700

    updated schemq.sql and data.sql

---
+++ database/data.sql
@@ -0,0 +1,2 @@
+insert into "users" ("userId", "nickname", "hashedPassword")
+values ('000', 'admin', 'admin');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,71 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "public"."posts" (
+	"postId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"url" TEXT NOT NULL,
+	"location" TEXT NOT NULL,
+	"caption" TEXT NOT NULL,
+	"uploadedAt" timestamptz(6) not null default now(),
+	CONSTRAINT "posts_pk" PRIMARY KEY ("postId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"nickname" TEXT NOT NULL UNIQUE,
+	"hashedPassword" TEXT NOT NULL,
+	"createdAt" timestamptz(6) not null default now(),
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."chats" (
+	"channel_url" TEXT NOT NULL,
+	"createdAt" timestamptz(6) not null default now(),
+	CONSTRAINT "chats_pk" PRIMARY KEY ("channel_url")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."favorites" (
+	"userId" integer NOT NULL,
+	"postId" integer NOT NULL,
+	"favoritedAt" timestamptz(6) not null default now()
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."pictures" (
+	"pictureId" serial NOT NULL,
+	"postId" serial NOT NULL,
+	"userId" serial NOT NULL,
+	CONSTRAINT "pictures_pk" PRIMARY KEY ("pictureId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+
+
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
+
+ALTER TABLE "pictures" ADD CONSTRAINT "pictures_fk0" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
+ALTER TABLE "pictures" ADD CONSTRAINT "pictures_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
