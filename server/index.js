55b39fa87c2b52a52e9d1176783bf5b005585add
Paul Park <paulpark330@hotmail.com>
2021-09-07 18:00:56 -0700

    working on server/index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,31 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const pg = require('pg');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const ClientError = require('./client-error');
+const jsonMiddleware = express.json();
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
+
+app.use(jsonMiddleware);
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+app.post('/api/home', (req, res, next) => {
+  const { userId } = req.user;
+  const { title, category, price, description } = req.body;
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
