7901afe4d69c553c4f0ccb9d43cce3eee007ef75
Paul Park <paulpark330@hotmail.com>
2021-09-19 22:46:05 -0700

    installed sendbird app

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -1,10 +1,31 @@
 import React, { useEffect, useContext } from 'react';
+import {
+  App as SendBirdApp,
+  SendBirdProvider,
+  withSendBird,
+  ChannelList,
+  Channel
+} from 'sendbird-uikit';
 import AppContext from '../lib/app-context';
 
 export default function Chat() {
+  const { username, userId } = useContext(AppContext);
+
   const { setPageTitle } = useContext(AppContext);
   useEffect(() => {
     setPageTitle('Chat');
   }, []);
-  return <div></div>;
+
+  const appId = 'ADE84D52-B56A-457C-9E88-47B8737C4DF5';
+  const nickname = username;
+
+  return (
+    <div>
+      <SendBirdApp
+        appId={appId}
+        userId={userId.toString()}
+        nickname={nickname}
+      />
+    </div>
+  );
 }
